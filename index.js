c4e566ce585ffe5a75c5a4dcd0d57583cabf6a2d
RemyJohnny <ewerejohnnyjnr93@gmail.com>
2023-02-22 21:10:36 +0300

    complete project

--- index.js
+++ index.js
@@ -1,7 +1,121 @@
 
-//let determinant= Math.floor(Math.random()*100);
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const userRecord = document.querySelector(".playersRecord");
+const botRecord = document.querySelector(".computersRecord");
+const scoreTitle = document.querySelector(".scoreHeader");
+const title = document.querySelector('.option-header');
+const rockimg = document.querySelector('.rockimg');
+const paperimg = document.querySelector('.paperimg');
+const scissorsimg = document.querySelector('.scissorsimg');
+const divs = document.querySelectorAll('.div-img');
+const optionsdiv = document.querySelector('.playersoptions');
+//button to reload the page after a complete round
+const btn = document.createElement('button');
+//parent element to appendthe child elemet(btn)
+const gamediv = document.querySelector('#gamesection');
+
+
+
+let playersChoice;
+let determinant;
+let computersChoice;
+let round;
+let computersRecord = 0;
+let playersRecord = 0 ;
+
+rock.addEventListener("click",() => {
+    rockimg.classList.add('clickedimg');
+    title.textContent ='^';
+    title.style.cssText ='color : rgb(236, 201, 112)'
+    playersChoice = "rock";
+    determinant= Math.ceil(Math.random()*12);
+    computersChoice = getComputersChoice(determinant);
+    round = playRound(playersChoice,computersChoice);
+    scorer(round);
+    endGame(playersRecord,computersRecord);
+});
+rockimg.addEventListener('transitionend', () => {
+    rockimg.classList.remove('clickedimg')
+})
+paperimg.addEventListener('transitionend', () => {
+    paperimg.classList.remove('clickedimg')
+})
+scissorsimg.addEventListener('transitionend', () => {
+    scissorsimg.classList.remove('clickedimg')
+})
+
+
+
+paper.addEventListener("click",() => {
+    paperimg.classList.add('clickedimg');
+    title.textContent ='^ ^';
+    title.style.cssText ='color : rgb(236, 201, 112)'
+    playersChoice = "paper";
+    determinant= Math.ceil(Math.random()*12);
+    computersChoice = getComputersChoice(determinant);
+    round = playRound(playersChoice,computersChoice);
+    scorer(round);
+    endGame(playersRecord,computersRecord);
+});
+scissors.addEventListener("click",() => {
+    scissorsimg.classList.add('clickedimg');
+    title.textContent ='^ ^ ^';
+    title.style.cssText ='color : rgb(236, 201, 112)'
+    playersChoice = "scissors";
+    determinant= Math.ceil(Math.random()*12);
+    computersChoice = getComputersChoice(determinant);
+    round = playRound(playersChoice,computersChoice);
+    scorer(round);
+    endGame(playersRecord,computersRecord);
+});
+//
+btn.addEventListener('click', () => {
+    location.reload();
+})
+
+function endGame(pC,cC){
+    if(pC === 7 && pC > cC){
+        rockimg.classList.add('fade');
+        scissorsimg.classList.add('fade');
+        paperimg.classList.add('fade');
+        optionsdiv.innerHTML = 'congratulations';
+        optionsdiv.style.cssText= 'color :green; font-weight: 1000; text-align: center; font-size: 40px; letter-spacing: 2px;'
+        gamediv.appendChild(btn);
+        btn.textContent = 'play again';
+    }
+    else if (cC === 7 && cC > pC){
+        rockimg.classList.add('fade');
+        scissorsimg.classList.add('fade');
+        paperimg.classList.add('fade');
+        console.log('computer won');
+        optionsdiv.innerHTML = 'you lose';
+        optionsdiv.style.cssText= 'color :red; font-weight: 1000; text-align: center; font-size: 40px;'
+        gamediv.appendChild(btn)
+        btn.textContent = 'try again';
+    }
+}
+
+
+
+function scorer (round){
+    if (round === 'l'){
+        computersRecord++;
+        botRecord.textContent = computersRecord;
+    }
+    else if (round ==='w'){
+        playersRecord++;
+        userRecord.textContent = playersRecord;
+    }
+    else if (round === 'wl'){
+        botRecord.textContent = computersRecord;
+        userRecord.textContent = playersRecord;
+    }
+}
 
 function getComputersChoice (determinant) {
+    //funtion to determine the computers choice
     if (determinant <= 4 ){
         return 'scissors' ;
     }
@@ -15,40 +129,49 @@ function getComputersChoice (determinant) {
 
 function playRound (playersChoice,computersChoice){
     if (playersChoice === 'rock' && computersChoice === 'scissors'){
-        console.log('You win... rock beats scissors ')
+        scoreTitle.textContent = 'You win... rock beats scissors ';
+        scoreTitle.style.color = "green"
         return 'w';
     }
     else if  (playersChoice === 'rock' && computersChoice === 'paper'){
-        console.log('You lose... paper beats rock');
+        scoreTitle.textContent = 'You lose... paper beats rock';
+        scoreTitle.style.color = 'red'
         return 'l';
     }
     else if  (playersChoice === 'rock' && computersChoice === 'rock'){
-        console.log("it,s a tie");
+        scoreTitle.textContent = "it's a tie";
+        scoreTitle.style.cssText ='colour: rgb(236, 201, 112)';
         return 'wl';
     }
     else if  (playersChoice === 'paper' && computersChoice === 'paper'){
-        console.log("it,s a tie");
+        scoreTitle.textContent = "it's a tie";
+        scoreTitle.style.cssText ='colour: rgb(236, 201, 112)';
         return 'wl';
 
     }
     else if  (playersChoice === 'paper' && computersChoice === 'scissors'){
-        console.log('You lose... scissors beats paper');
+        scoreTitle.textContent = 'You lose... scissors beats paper';
+        scoreTitle.style.color = 'red'
         return 'l';
     }
     else if  (playersChoice === 'paper' && computersChoice === 'rock'){
-        console.log('You win... paper beats rock');
+        scoreTitle.textContent = 'You win... paper beats rock';
+        scoreTitle.style.color = "green";
         return 'w';
     }
     else if  (playersChoice === 'scissors' && computersChoice === 'paper'){
-        console.log('You win...  scissors beats paper');
+        scoreTitle.textContent = 'You win...  scissors beats paper';
+        scoreTitle.style.color = "green";
         return 'w';
     }
     else if  (playersChoice === 'scissors' && computersChoice === 'rock'){
-        console.log('You lose... rock beats scissors');
+        scoreTitle.textContent = 'You lose... rock beats scissors';
+        scoreTitle.style.color = 'red'
         return 'l';
     }
     else if (playersChoice === 'scissors' && computersChoice === 'scissors'){
-        console.log('it\'s a tie');
+        scoreTitle.textContent = 'it\'s a tie';
+        scoreTitle.style.cssText ='colour: rgb(236, 201, 112)';
         return 'wl';
     }
     else {
@@ -58,49 +181,4 @@ function playRound (playersChoice,computersChoice){
 
 
 
-}
-let computersRecord = 0;
-let playersRecord = 0 ;
-
-
-function game() {
-
-    for(i = 0; i < 5; i++){
-        const playersinput = prompt ( 'rock , paper or scissors?');
-        let playersChoice = playersinput.toLowerCase();
-        let determinant= Math.floor(Math.random()*12);
-        let computersChoice = getComputersChoice(determinant);
-        let round = playRound(playersChoice,computersChoice);
-        
-        if (round === 'l'){
-            computersRecord++;
-        }
-        else if (round ==='w'){
-            playersRecord++;
-        }
-        else if (round === 'wl'){
-            computersRecord++;
-            playersRecord++;
-        }
-        /*console.log(determinant);
-        console.log('players choice = '+ playersChoice);
-        console.log('computers choice = ' + computersChoice);*/
-        console.log('computers record  = ' + computersRecord);
-        console.log('players record = ' + playersRecord);
-        
-    }
-}
-
-game();
-console.log('.....');
-console.log('computers record  = ' + computersRecord);
-console.log('players record = ' + playersRecord)
-if ( playersRecord > computersRecord){
-    console.log('YOU WON');
-}
-else if (playersRecord < computersRecord){
-    console.log('YOU LOSE');
-}
-else {
-    console.log("it's a tie");
 }
